896746aa8f2d5457d7ea3f5189ee8fbb5355bb09
bhandarisatkriti <satkriti32@gmail.com>
2026-06-24 08:57:18 +0545

    Auth Updated

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -8,7 +8,11 @@ from apps.shared.constants import ROLE_JOB_SEEKER, USER_ROLES
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     profile_picture = models.TextField(blank=True, default="")
-    role = models.CharField(max_length=20, choices=USER_ROLES, default=ROLE_JOB_SEEKER)
+    role = models.CharField(
+        max_length=20,
+        choices=USER_ROLES,
+        default=ROLE_JOB_SEEKER
+    )
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
@@ -18,7 +22,7 @@ class User(AbstractUser):
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name="profile")
     skills = models.JSONField(default=list, blank=True)
     resume_text = models.TextField(blank=True)
     cv_url = models.URLField(blank=True)
